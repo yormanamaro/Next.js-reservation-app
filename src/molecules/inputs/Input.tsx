fe3254045bfeec29bac1faf5d6eabaa492a69710
Yorman Eduardo Amaro Carrero <yormaneduardoamarocarrero@yormans-mbp.lan>
2023-08-04 16:45:25 -0400

    Modal input style correction

--- src/molecules/inputs/Input.tsx
+++ src/molecules/inputs/Input.tsx
@@ -37,7 +37,7 @@ const Input: React.FC<InputProps> = ({
         ${errors[id] ? "focus:border-rose-500" : "focus:border-black"}
       `}
     />
-    <label className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-4 ${errors[id] ? "text-rose-500" : "text-zinc-400"} `}>
+    <label className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] ${formatPrice ? "left-9": "left-4"} peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-4 ${errors[id] ? "text-rose-500" : "text-zinc-400"} `}>
       {label}
     </label>
   </div>
